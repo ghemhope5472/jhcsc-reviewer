6d109a5354bf621d029f3dad26c915f1bb3c9483
Glenn <ghemhope@gmail.com>
2021-04-29 10:26:32 +0800

    change oauth keys

--- config/keys.js
+++ config/keys.js
@@ -8,7 +8,10 @@ dbPassword='mongodb+srv://ghemhope:ghemhope@cluster0.n1xnq.mongodb.net/myFirstDa
 module.exports = {
     mongoURI: dbPassword,
     google: {
-        clientID : '765952251939-6b7m92o2p1tt0chfk8mp96oh9n123nsc.apps.googleusercontent.com',
-        clientSecret: '40xu8H_serQ6fg6cDqfhJtdI'
+        // clientID : '765952251939-6b7m92o2p1tt0chfk8mp96oh9n123nsc.apps.googleusercontent.com',
+        clientID : '696915403702-hceekb6sg8gclpvktfspieu5f54dolch.apps.googleusercontent.com',
+        // clientSecret: '40xu8H_serQ6fg6cDqfhJtdI',
+        clientSecret: 'csI96-2PeEOAsL9nGeQHctTd'
+
     }
 };
